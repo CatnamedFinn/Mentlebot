4564c38e9342043b72f93dd0989fb7cfe1f80ed5
CatnamedFinn <69591543+CatnamedFinn@users.noreply.github.com>
2021-10-22 23:19:12 +0700

    chore: token update

--- bot/Mentlebot.py
+++ bot/Mentlebot.py
@@ -5,8 +5,6 @@ client = discord.Client()
 @client.event
 async def onReady():
     print("We have logged in as {0.user}".format(client))
-
-        
     
 @client.event
 async def onMessage(message):
@@ -16,4 +14,4 @@ async def onMessage(message):
         await message.channel.send("Hello")
     
 
-client.run("OTAwOTk2MDk0MDg1OTc2MDc2.YXJcAA.lju4yKrgWfvUdwIHCAPCfB2BNzo")
+client.run("OTAwOTk2MDk0MDg1OTc2MDc2.YXJcAA.oySi9gjpWmh7MJRiSvvPoXipqrs")
