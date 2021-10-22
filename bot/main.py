4a184ad4ef62722b681a87c344a51a630b643423
CatnamedFinn <69591543+CatnamedFinn@users.noreply.github.com>
2021-10-23 00:12:02 +0700

    chore: bot token update

--- bot/main.py
+++ bot/main.py
@@ -10,8 +10,13 @@ async def onReady():
 async def onMessage(message):
     if message.author == client.user:
         return
+    fuckDetector(message)
     if message.content.startswith("!hello"):
         await message.channel.send("Hello")
     
 
-client.run("OTAwOTk2MDk0MDg1OTc2MDc2.YXJcAA.oySi9gjpWmh7MJRiSvvPoXipqrs")
+@client.event
+async def fuckDetector(message):
+    await message.channel.send(message.replace("fuck", ""))
+
+client.run("OTAwOTk2MDk0MDg1OTc2MDc2.YXJcAA.vlgFeBnER7dsd5GjFznP3vP09Q8")
