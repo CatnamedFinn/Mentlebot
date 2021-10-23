7100749cf3589d42a3c8fc6022557a027ce686bd
CatnamedFinn <69591543+CatnamedFinn@users.noreply.github.com>
2021-10-23 21:38:11 +0700

    chore: no wall of text

--- bot/main.py
+++ bot/main.py
@@ -125,21 +125,21 @@ async def _3d3t(ctx, arg1, arg2):
 
 
 @bot.command()
-async def about(ctx):
+async def about(ctx, arg):
     embed = discord.Embed(title = "What the frick is Mentlebot?", description = "Hello fellow user, I am Mentlebot, a joint project between two IT random furries made with the goal for coding practice and fun.", color = discord.Color.blurple())
     
     embed.set_thumbnail(url = "https://preview.redd.it/jpzji9ml0rq71.jpg?auto=webp&s=b005458162a35f0275d7edf8f5b15e7617efd983")
 
     embed.add_field(name = "Features", value = "Below are the list of commands I can perform.", inline = False)
-    embed.add_field(name = "WD Ghost-Free", value = "Anti ghosting feature. Members @'ed within the command must reply to the original message or be greeted with an n number of messages and @s from the bot. The message contents and the number of messages sent can be configured.", inline = True)
-    embed.add_field(name = "Party manager" , value = "Game party tracking feature. @s a party role and tracks the number of subscribed members that replied to the @, closing the original @ once the maximum number of members replying to it is met. Party role and maximum number of members can be configured." , inline = True)
-    embed.add_field(name = "Stuffify" , value = "Creates and sends an 'I'm Stuff' macro meme with custom text.", inline = True)
-    embed.add_field(name = "Adit", value = "Command that sends a DM and/or @s the member 'Adit'", inline = True)
-    embed.add_field(name = "Adit.int", value = "Posts a picture of the member 'Adit' inting or feeding. Gallery for said pictures is available at ___.", inline = True)
-    embed.add_field(name = "Team.int", value = "Posts a picture of an entire team inting or feeding. Gallery for said pictures is available at ___.", inline = True)
-    embed.add_field(name = "4k", value = "Posts an extremely sus picture. Gallery for said pictures is available at ___.", inline = True)
-    embed.add_field(name = "Yo like the?", value = "Custom automatic reply feature. The bot replies to a message containing a custom keyword with 'You like the <custome message>'.", inline = True)
-    embed.add_field(name = "Susfinder", value = "Replies to the command message with a message containing only the characters 's', 'u', and 's' in order.", inline = True)
+    embed.add_field(name = "WD Ghost-Free", value = "Anti ghosting feature.", inline = True)
+    embed.add_field(name = "Party manager" , value = "Game party tracking feature." , inline = True)
+    embed.add_field(name = "Stuffify" , value = "Posts custom 'I'm stuff' images", inline = True)
+    embed.add_field(name = "Adit", value = "@s or DMs the member 'Adit'", inline = True)
+    embed.add_field(name = "Adit.int", value = "Posts a picture of Adit inting or feeding", inline = True)
+    embed.add_field(name = "Team.int", value = "Posts a picture of an entire team inting or feeding.", inline = True)
+    embed.add_field(name = "4k", value = "Posts an extremely sus picture.", inline = True)
+    embed.add_field(name = "Yo like the?", value = "Bot responds to a keyword with a custom message.", inline = True)
+    embed.add_field(name = "Susfinder", value = "Highlights 's', 'u' and 's' in a message", inline = True)
     embed.add_field(name = "Tic tac toe", value = "I'm pretty sure you know what this is.", inline = True)
 
     embed.set_footer(text = "This is a personal project so don't take this seriously.")
