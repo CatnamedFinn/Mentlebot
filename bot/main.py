8b4acdc051476b1fd9a8cb048cc12b1c090b9d5b
Alexander Khrisna Satyadhana <alex.khrisna.s@gmail.com>
2021-10-23 21:02:35 +0700

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -3,12 +3,23 @@ import os
 import re
 
 from discord.ext import commands
+from enum import Enum
 
 token = os.environ.get('DISCORD_TOKEN')
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-tttBoard = [
+
+class TicTacToeStateType(Enum):
+    NOT_PLAYING = 1
+    LOBBYING = 2
+    PLAYING = 3
+
+
+ticTacToeState = TicTacToeStateType.INACTIVE
+ticTacToePlayers = []
+ticTacToeTurn = 0
+ticTacToeBoard = [
     [' ', ' ', ' '],
     [' ', ' ', ' '],
     [' ', ' ', ' ']
@@ -34,8 +45,13 @@ async def on_message(message):
 
 
 @bot.command()
-async def sus(ctx):
+async def sussify(ctx):
     message = ctx.message
+
+    if message.reference is None:
+        await ctx.send("You need to reply to a message in order to sussify it.")
+        return
+
     replyId = message.reference.message_id
     reply = await ctx.channel.fetch_message(replyId)
     content = reply.content
@@ -47,13 +63,50 @@ async def sus(ctx):
 
 
 @bot.command(name='3d3t')
-async def _3d3t(ctx):
+async def _3d3t(ctx, arg1, arg2):
+    global ticTacToeState, ticTacToePlayers, ticTacToeTurn, ticTacToeBoard
+
+    if ticTacToeState == TicTacToeStateType.NOT_PLAYING:
+        # start lobbying
+        ticTacToeState = TicTacToeStateType.LOBBYING
+        ticTacToePlayers.insert(ctx.author)
+        return
+
+    if ticTacToeState == TicTacToeStateType.LOBBYING:
+        # keep lobbying
+        playerCount = len(ticTacToePlayers)
+        ticTacToePlayers.insert(ctx.author)
+        await ctx.send(f"{ctx.author.display_name} has joined as Player {playerCount}!")
+
+        if playerCount == 2:
+            await ctx.send("Max players reached. Starting game now!")
+            ticTacToeState = TicTacToeStateType.PLAYING
+        return
+
+    if ticTacToeState == TicTacToeStateType.PLAYING:
+        # end the game
+        if arg1 == 'stop':
+            await ctx.send("Ending game now...")
+            ticTacToeState = TicTacToeStateType.NOT_PLAYING
+            return
+        # wrong player
+        if ctx.author is not ticTacToePlayers[ticTacToeTurn]:
+            await ctx.send("Now's not your turn.")
+            return
+
+        await ctx.send("A game is already playing!")
+        return
+
+    board = ''
+    for row in ticTacToeBoard:
+        board += f"| {row[0]} | {row[1]} | {row[2]} |\n"
+
     await ctx.send(
         "```"
-        "X | X | O\n"
-        "X |   | O\n"
-        "O | X | X\n"
+        "Player 1 has made their move!"
+        f"{board}"
         "```"
+        "Player 2, specify your next coordinates."
     )
 
 bot.run(token)
