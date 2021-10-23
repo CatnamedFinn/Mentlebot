668b6772b2b3d0312377823397c0d3c5b7770ba4
CatnamedFinn <69591543+CatnamedFinn@users.noreply.github.com>
2021-10-23 21:29:36 +0700

    chore: about embed command

--- bot/main.py
+++ bot/main.py
@@ -119,9 +119,11 @@ async def _3d3t(ctx, arg1, arg2):
 
 
 @bot.command()
-async def tortilla(ctx):
+async def about(ctx):
     embed = discord.Embed(title = "What the frick is Mentlebot?", description = "Hello fellow user, I am Mentlebot, a joint project between two IT random furries made with the goal for coding practice and fun.", color = discord.Color.blurple())
     
+    embed.set_thumbnail(url = "https://preview.redd.it/jpzji9ml0rq71.jpg?auto=webp&s=b005458162a35f0275d7edf8f5b15e7617efd983")
+
     embed.add_field(name = "Features", value = "Below are the list of commands I can perform.", inline = False)
     embed.add_field(name = "WD Ghost-Free", value = "Anti ghosting feature. Members @'ed within the command must reply to the original message or be greeted with an n number of messages and @s from the bot. The message contents and the number of messages sent can be configured.", inline = True)
     embed.add_field(name = "Party manager" , value = "Game party tracking feature. @s a party role and tracks the number of subscribed members that replied to the @, closing the original @ once the maximum number of members replying to it is met. Party role and maximum number of members can be configured." , inline = True)
@@ -132,8 +134,9 @@ async def tortilla(ctx):
     embed.add_field(name = "4k", value = "Posts an extremely sus picture. Gallery for said pictures is available at ___.", inline = True)
     embed.add_field(name = "Yo like the?", value = "Custom automatic reply feature. The bot replies to a message containing a custom keyword with 'You like the <custome message>'.", inline = True)
     embed.add_field(name = "Susfinder", value = "Replies to the command message with a message containing only the characters 's', 'u', and 's' in order.", inline = True)
+    embed,add_field(name = "Tic tac toe", value = "I'm pretty sure you know what this is.", inline = True)
 
-    embed.set.footer(text = "This is a personal project so don't take this seriously.")
+    embed.set_footer(text = "This is a personal project so don't take this seriously.")
     
     await ctx.send(embed = embed)
 
