158176541aea569df9f13c9c24432e118bd95c01
Alexander Khrisna Satyadhana <alex.khrisna.s@gmail.com>
2021-10-27 21:14:22 +0700

    funi

--- bot/commands/almond.py
+++ bot/commands/almond.py
@@ -1,21 +1,12 @@
 import discord
 from discord.ext import commands
 
-bot = None
-
 
 @commands.command()
 async def almond(ctx):
-    if bot != None:
-        message_channel = bot.get_channel(901007512311070731)
-        await ctx.send(message_channel)
-        await message_channel.send('Ken Nara Gaza Omori')
-    else:
-        await ctx.send("I like almond-scented channels.")
+    await ctx.send("I like almond-scented channels.")
 
 
 def setup(parent_bot):
-    global bot
     # Every extension should have this function
     parent_bot.add_command(almond)
-    bot = parent_bot
